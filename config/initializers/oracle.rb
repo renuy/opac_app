5cb8581177788b97bc21ca2fc979f62476508c07
akhilesh <kataria.subs@gmail.com>
2011-01-10 09:50:29 +0530

    setting oracle enhanced_adaptor's TZ to UTC

--- config/initializers/oracle.rb
+++ config/initializers/oracle.rb
@@ -1,3 +1,5 @@
+ENV['TZ'] = 'UTC'
+
 ActiveSupport.on_load(:active_record) do
   ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter.class_eval do
     self.emulate_integers_by_column_name = true
