d8f0ddf0e96d4b3e3d96fdd2a5984b85422dd51f
akhilesh <kataria.subs@gmail.com>
2011-01-17 17:28:27 +0530

    mails to be sent from strataretail.co

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -1,6 +1,6 @@
 class SignupMailer < ActionMailer::Base
   default :from => "mc@strataretail.co"
-
+  default :reply_to => "mc@justbooksclc.com"
   def registration_confirmation(signup)
     @url = 'http://www.justbooksclc.com'
     @signup = signup
