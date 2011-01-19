f94e3f91e1d8af4d5ae3c19bf9af51656da99683
akhilesh <kataria.subs@gmail.com>
2011-01-19 15:42:57 +0530

    removed unused function

--- app/controllers/ibtrs_controller.rb
+++ app/controllers/ibtrs_controller.rb
@@ -33,40 +33,6 @@ class IbtrsController < ApplicationController
       @f_errors = []
     end
   end
-  
-  def fulfill_old
-    book_no = params[:bookno]
-    branch_id = params[:branch_id]
-    if branch_id.nil?
-      branch_id = 951
-    end
-    @f_errors =[]
-    
-    @ibtr = Ibtr.find_for_fulfill(book_no)
-    if !@ibtr.nil?
-      
-        if @ibtr.set_fulfill(book_no, branch_id)
-          respond_to do |format|
-            flash[:notice] = "book fulfilled to #{@ibtr.card_id} of branch #{@ibtr.branch}"
-            format.html { redirect_to(@ibtr, :notice => "book fulfilled to #{@ibtr.card_id} of branch #{@ibtr.branch}") }
-            format.xml  { head :ok }
-          end
-          return
-        else
-            @f_errors << @ibtr.errors
-        end
-      
-    else
-      @f_errors << "Book does not match any IBTR."
-    end  
-    
-    @ibtr = Ibtr.find_by_book_no(book_no)  
-    #render 'lookup'
-    respond_to do |format|
-      format.html { render "/ibtrs/lookup" ,  :f_error => 'true' }
-      format.xml  { render :xml => @f_error, :status => :unprocessable_entity }
-    end
-  end
 
   def fulfill
     book_no = params[:bookno]
--- db/schema.rb
+++ db/schema.rb
@@ -10,54 +10,151 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110118130459) do
+ActiveRecord::Schema.define(:version => 20110117125146) do
+
+  create_table "addresses", :force => true do |t|
+    t.string   "address",     :limit => 900
+    t.string   "locality",    :limit => 200
+    t.string   "city",        :limit => 100
+    t.string   "state",       :limit => 100
+    t.string   "country",     :limit => 100
+    t.integer  "customer_id"
+    t.integer  "pincode"
+    t.integer  "mphone"
+    t.integer  "ophone"
+    t.integer  "hphone"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "authentications", :force => true do |t|
-    t.integer  "user_id",    :precision => 38, :scale => 0
+    t.integer  "user_id"
     t.string   "provider"
     t.string   "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  create_table "authors", :force => true do |t|
+    t.string   "name"
+    t.string   "firstname"
+    t.string   "middlename"
+    t.string   "lastname"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  # unrecognized index "index_authors_on_name" with type ActiveRecord::ConnectionAdapters::IndexDefinition
+
+  create_table "books", :force => true do |t|
+    t.string   "book_no"
+    t.string   "shelf_location"
+    t.integer  "branch_id"
+    t.integer  "catalogued_branch_id"
+    t.string   "state"
+    t.integer  "title_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  # unrecognized index "index_books_on_book_no" with type ActiveRecord::ConnectionAdapters::IndexDefinition
+
+  create_table "branches", :force => true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "is_a"
+    t.integer  "parent_id"
+    t.string   "parent_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "card_id"
+  end
+
+  create_table "cards", :force => true do |t|
+    t.string   "number",        :limit => 9
+    t.integer  "membership_id"
+    t.string   "status",        :limit => 2
+    t.date     "date_lost"
+    t.date     "date_issued"
+    t.date     "date_returned"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "division"
+  end
+
+  # unrecognized index "index_categories_on_name_and_division" with type ActiveRecord::ConnectionAdapters::IndexDefinition
+
+  create_table "cities", :force => true do |t|
+    t.string   "cityname"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "consignments", :force => true do |t|
-    t.string    "consignor"
-    t.string    "consignee"
-    t.integer   "origin_id",                          :precision => 38, :scale => 0
-    t.integer   "destination_id",                     :precision => 38, :scale => 0
-    t.string    "waybill_no"
-    t.string    "origin_address"
-    t.string    "destination_address"
-    t.integer   "goods_count",                        :precision => 38, :scale => 0
-    t.integer   "goods_delivered_count",              :precision => 38, :scale => 0
-    t.string    "state"
-    t.timestamp "pickup_date",           :limit => 6
-    t.timestamp "delivery_date",         :limit => 6
-    t.datetime  "created_at"
-    t.datetime  "updated_at"
+    t.string   "consignor"
+    t.string   "consignee"
+    t.integer  "origin_id"
+    t.integer  "destination_id"
+    t.string   "waybill_no"
+    t.string   "origin_address"
+    t.string   "destination_address"
+    t.integer  "goods_count"
+    t.integer  "goods_delivered_count"
+    t.string   "state"
+    t.datetime "pickup_date"
+    t.datetime "delivery_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "coupons", :force => true do |t|
     t.string   "name"
-    t.integer  "amount",     :precision => 38, :scale => 0
+    t.decimal  "amount",     :precision => 10, :scale => 0
     t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "discount"
+    t.decimal  "discount",   :precision => 10, :scale => 0
   end
 
   create_table "coupons_plans", :id => false, :force => true do |t|
-    t.integer  "coupon_id",  :precision => 38, :scale => 0
-    t.integer  "plan_id",    :precision => 38, :scale => 0
+    t.integer  "coupon_id"
+    t.integer  "plan_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "customers", :force => true do |t|
+    t.string   "title"
+    t.string   "name"
+    t.string   "gender",                :limit => 2
+    t.date     "date_of_birth"
+    t.string   "email_id"
+    t.integer  "address_id"
+    t.string   "general_info_level",    :limit => 2
+    t.string   "email_info_level",      :limit => 2
+    t.string   "address_info_level",    :limit => 2
+    t.string   "book_shelf_info_level", :limit => 2
+    t.string   "groups_info_level",     :limit => 2
+    t.string   "favorites_info_level",  :limit => 2
+    t.string   "customer_type",         :limit => 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "goods", :force => true do |t|
     t.string   "book_no"
-    t.integer  "title_id",       :precision => 38, :scale => 0
-    t.integer  "ibtr_id",        :precision => 38, :scale => 0
-    t.integer  "consignment_id", :precision => 38, :scale => 0
+    t.integer  "title_id"
+    t.integer  "ibtr_id"
+    t.integer  "consignment_id"
     t.string   "state"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -69,135 +166,188 @@ ActiveRecord::Schema.define(:version => 20110118130459) do
     t.string   "shelf"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "respondent_id", :precision => 38, :scale => 0
-    t.integer  "title_id",      :precision => 38, :scale => 0
+    t.integer  "respondent_id"
+    t.integer  "title_id"
   end
 
   create_table "ibt_search_criterias", :force => true do |t|
-    t.integer   "respondent_id",              :precision => 38, :scale => 0
-    t.timestamp "time_basis",    :limit => 6
-    t.datetime  "created_at"
-    t.datetime  "updated_at"
+    t.integer  "respondent_id"
+    t.datetime "time_basis"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "ibtr_versions", :force => true do |t|
-    t.integer   "ibtr_id",                    :precision => 38, :scale => 0
-    t.integer   "version",                    :precision => 38, :scale => 0
-    t.integer   "title_id",                   :precision => 38, :scale => 0
-    t.integer   "member_id",                  :precision => 38, :scale => 0
-    t.string    "card_id"
-    t.integer   "branch_id",                  :precision => 38, :scale => 0
-    t.integer   "rns_id",                     :precision => 38, :scale => 0
-    t.string    "state"
-    t.timestamp "created_at",    :limit => 6
-    t.timestamp "updated_at",    :limit => 6
-    t.string    "respondent_id"
-    t.integer   "reason_id",                  :precision => 38, :scale => 0
-    t.string    "comments"
-    t.string    "book_no"
+    t.integer  "ibtr_id"
+    t.integer  "version"
+    t.integer  "title_id"
+    t.integer  "member_id"
+    t.string   "card_id"
+    t.integer  "branch_id"
+    t.integer  "rns_id"
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "respondent_id"
+    t.integer  "reason_id"
+    t.string   "comments"
   end
+
+  # unrecognized index "index_ibtr_versions_on_ibtr_id" with type ActiveRecord::ConnectionAdapters::IndexDefinition
 
   create_table "ibtrs", :force => true do |t|
-    t.integer   "title_id",                   :precision => 38, :scale => 0
-    t.integer   "member_id",                  :precision => 38, :scale => 0
-    t.string    "card_id"
-    t.integer   "branch_id",                  :precision => 38, :scale => 0
-    t.integer   "rns_id",                     :precision => 38, :scale => 0
-    t.string    "state"
-    t.timestamp "created_at",    :limit => 6
-    t.timestamp "updated_at",    :limit => 6
-    t.integer   "version",                    :precision => 38, :scale => 0
-    t.string    "respondent_id"
-    t.integer   "reason_id",                  :precision => 38, :scale => 0
-    t.string    "comments"
-    t.string    "book_no"
+    t.integer  "title_id"
+    t.integer  "member_id"
+    t.string   "card_id"
+    t.integer  "branch_id"
+    t.integer  "rns_id"
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "version"
+    t.string   "respondent_id"
+    t.integer  "reason_id"
+    t.string   "comments"
   end
 
-  add_index "ibtrs", ["card_id"], :name => "in_ibtrs_3"
-  add_index "ibtrs", ["member_id"], :name => "in_ibtrs_4"
-  add_index "ibtrs", ["rns_id"], :name => "in_ibtrs_1"
-  add_index "ibtrs", ["state"], :name => "in_ibtrs_2"
+  create_table "memberpayments", :force => true do |t|
+    t.integer  "membership_id"
+    t.integer  "customer_id"
+    t.integer  "card_id"
+    t.decimal  "amount",                           :precision => 10, :scale => 0
+    t.string   "currency",          :limit => 4
+    t.string   "transaction_type",  :limit => 50
+    t.integer  "txn_branch_id"
+    t.integer  "account_branch_id"
+    t.integer  "plan_id"
+    t.string   "remarks",           :limit => 300
+    t.integer  "payment_mode"
+    t.integer  "original_id"
+    t.string   "status",            :limit => 4
+    t.string   "payment_details",   :limit => 100
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "plans", :force => true do |t|
     t.string   "name"
-    t.decimal  "security_deposit"
-    t.decimal  "registration_fee"
-    t.decimal  "reading_fee"
-    t.decimal  "magazine_fee"
-    t.integer  "num_books",        :precision => 38, :scale => 0
-    t.integer  "num_magazines",    :precision => 38, :scale => 0
+    t.float    "security_deposit"
+    t.float    "registration_fee"
+    t.float    "reading_fee"
+    t.float    "magazine_fee"
+    t.integer  "num_books"
+    t.integer  "num_magazines"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "reading_limit",    :precision => 38, :scale => 0
-    t.boolean  "subscription",     :precision => 1,  :scale => 0
+    t.integer  "reading_limit"
+    t.boolean  "subscription"
   end
 
-  create_table "read_next_shelf", :id => false, :force => true do |t|
-    t.integer   "memberid",         :limit => 10,  :precision => 10, :scale => 0
-    t.string    "membercardno",     :limit => 15
-    t.integer   "bookid",           :limit => 10,  :precision => 10, :scale => 0
-    t.timestamp "daterequested",    :limit => 6
-    t.string    "status",           :limit => 50
-    t.timestamp "dateissued",       :limit => 6
-    t.timestamp "datedelevered",    :limit => 6
-    t.timestamp "datecollected",    :limit => 6
-    t.integer   "readingperiority", :limit => 10,  :precision => 10, :scale => 0
-    t.integer   "titleid",          :limit => 10,  :precision => 10, :scale => 0
-    t.decimal   "requestid",                                                      :null => false
-    t.decimal   "requestingbranch"
-    t.decimal   "fulfillingbranch"
-    t.decimal   "slotnumber"
-    t.decimal   "drpassword"
-    t.decimal   "booknumber"
-    t.decimal   "reason"
-    t.string    "remarks",          :limit => 200
-    t.timestamp "iblastupdatedate", :limit => 6
-    t.string    "ddstatus",         :limit => 2
-    t.string    "procstatus",       :limit => 2
-    t.integer   "orderid",          :limit => 11,  :precision => 11, :scale => 0
-    t.datetime  "returndate"
-    t.string    "ponumber",         :limit => 200
+  create_table "publishers", :force => true do |t|
+    t.string   "name"
+    t.string   "country"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  # unrecognized index "index_publishers_on_name" with type ActiveRecord::ConnectionAdapters::IndexDefinition
 
   create_table "signups", :force => true do |t|
-    t.string   "name",                                              :null => false
-    t.string   "address",                                           :null => false
-    t.integer  "mphone",             :precision => 38, :scale => 0
-    t.integer  "lphone",             :precision => 38, :scale => 0
-    t.string   "email",                                             :null => false
+    t.string   "name",                                :null => false
+    t.string   "address",                             :null => false
+    t.integer  "mphone"
+    t.integer  "lphone"
+    t.string   "email",                               :null => false
     t.string   "referrer_member_id"
-    t.integer  "referrer_cust_id",   :precision => 38, :scale => 0
-    t.integer  "plan_id",            :precision => 38, :scale => 0, :null => false
-    t.integer  "branch_id",          :precision => 38, :scale => 0
-    t.integer  "signup_months",      :precision => 38, :scale => 0, :null => false
-    t.decimal  "security_deposit",                                  :null => false
-    t.decimal  "registration_fee",                                  :null => false
-    t.decimal  "reading_fee",                                       :null => false
-    t.decimal  "discount",                                          :null => false
-    t.decimal  "advance_amt",                                       :null => false
-    t.integer  "payment_mode",       :precision => 38, :scale => 0, :null => false
-    t.string   "payment_ref",                                       :null => false
+    t.integer  "referrer_cust_id"
+    t.integer  "plan_id",                             :null => false
+    t.integer  "branch_id"
+    t.integer  "signup_months",                       :null => false
+    t.float    "security_deposit",                    :null => false
+    t.float    "registration_fee",                    :null => false
+    t.float    "reading_fee",                         :null => false
+    t.float    "discount",                            :null => false
+    t.float    "advance_amt",                         :null => false
+    t.float    "paid_amt",                            :null => false
+    t.float    "overdue_amt",                         :null => false
+    t.integer  "payment_mode",                        :null => false
+    t.string   "payment_ref",                         :null => false
     t.string   "membership_no"
     t.string   "application_no"
     t.string   "employee_no"
-    t.integer  "created_by",         :precision => 38, :scale => 0, :null => false
-    t.integer  "modified_by",        :precision => 38, :scale => 0, :null => false
+    t.integer  "created_by",                          :null => false
+    t.integer  "modified_by",                         :null => false
+    t.string   "flag_migrated",      :default => "U"
+    t.date     "start_date",                          :null => false
+    t.date     "expiry_date",                         :null => false
+    t.string   "remarks"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "coupon_amt"
+    t.float    "coupon_amt"
     t.string   "coupon_no"
-    t.integer  "coupon_id",          :precision => 38, :scale => 0
+    t.integer  "coupon_id"
   end
+
+  create_table "states", :force => true do |t|
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "stock", :id => false, :force => true do |t|
+    t.integer "title_id"
+    t.integer "branch_id"
+    t.integer "in_store_cnt"
+    t.integer "in_circulation_cnt"
+    t.integer "unavailable_cnt"
+    t.integer "total_cnt"
+  end
+
+  create_table "stockitems", :force => true do |t|
+    t.integer "title_id"
+    t.integer "author_id"
+    t.integer "category_id"
+    t.integer "publisher_id"
+    t.string  "title_name"
+    t.string  "isbn"
+    t.decimal "price",        :precision => 10, :scale => 0
+    t.string  "language"
+    t.string  "edition"
+    t.string  "pubyear"
+    t.integer "pages"
+    t.string  "binding"
+  end
+
+  # unrecognized index "index_stockitems_on_title_id" with type ActiveRecord::ConnectionAdapters::IndexDefinition
+
+  create_table "titles", :force => true do |t|
+    t.string   "title",             :null => false
+    t.integer  "author_id"
+    t.integer  "publisher_id"
+    t.integer  "yearofpublication"
+    t.integer  "edition"
+    t.integer  "category_id"
+    t.string   "isbn10"
+    t.string   "isbn13"
+    t.integer  "noofpages"
+    t.string   "language"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  # unrecognized index "index_titles_on_author_id" with type ActiveRecord::ConnectionAdapters::IndexDefinition
+  # unrecognized index "index_titles_on_publisher_id" with type ActiveRecord::ConnectionAdapters::IndexDefinition
+  # unrecognized index "index_titles_on_title" with type ActiveRecord::ConnectionAdapters::IndexDefinition
 
   create_table "users", :force => true do |t|
     t.string   "username"
-    t.string   "email",                                                              :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128,                                :default => ""
-    t.string   "password_salt",                                                      :default => ""
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :precision => 38, :scale => 0, :default => 0
+    t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -205,5 +355,8 @@ ActiveRecord::Schema.define(:version => 20110118130459) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  # unrecognized index "index_users_on_reset_password_token" with type ActiveRecord::ConnectionAdapters::IndexDefinition
+  # unrecognized index "index_users_on_username" with type ActiveRecord::ConnectionAdapters::IndexDefinition
 
 end
