829a91947efee882b02c711eada7b20d995ef4d8
renuy <renu.yarday@strata.co.in>
2011-02-15 16:25:50 +0530

    added the view stock_racks_shelves for keepakes

--- db/views.sql
+++ db/views.sql
@@ -2,7 +2,7 @@ create or replace view ib_assignments as
 select
     b.title_id ,
     NVL(a.book_branch_id,b.respondent_id) book_branch_id,
-    NVL(a.category,'NK') category,
+    NVL(a.category,'UnKnown') category,
     NVL(a.rack,'R0') rack,
     NVL(a.shelf,'S0') shelf,
     b.respondent_id respondent_id,
@@ -28,9 +28,35 @@ select
     --b.branch_id ,
     --b.created_at ,
     b.id,
-    NVL(a.category,'NK'),
+    NVL(a.category,'UnKnown'),
     NVL(a.rack,'R0'),
     NVL(a.shelf,'S0')
+    with read only
+
+ ;
+
+create or replace view stock_racks_shelves as
+select
+    --a.bookid book_id,
+    --a.booknumber book_number,
+    a.titleid title_id,
+    a.location book_branch_id,
+    --a.shelflocation shelf_location,
+    --a.status book_status,
+    replace(substr(shelflocation, 1, length(shelflocation)- regexp_instr(shelflocation,'R\d')),'-','') Category,
+    replace(substr(shelflocation, regexp_instr(shelflocation,'R\d'), regexp_instr(shelflocation,'S\d')-regexp_instr(shelflocation,'R\d')),'-','') Rack,
+    substr(shelflocation, regexp_instr(shelflocation,'S\d'),25) Shelf,
+    count(1) count_per_shelf
+    from jbprod.books a
+    --where a.status != 'D'
+    group by
+    a.titleid ,
+    a.location ,
+    --a.shelflocation  ,
+    replace(substr(shelflocation, 1, length(shelflocation)- regexp_instr(shelflocation,'R\d')),'-',''),
+    replace(substr(shelflocation, regexp_instr(shelflocation,'R\d'), regexp_instr(shelflocation,'S\d')-regexp_instr(shelflocation,'R\d')),'-',''),
+    substr(shelflocation, regexp_instr(shelflocation,'S\d'),25)
+
     with read only
 
  ;
