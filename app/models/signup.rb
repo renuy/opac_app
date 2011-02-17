280a434ff79abedfe559f417286109b3f72d8614
Subhash Bhushan <subhash.bhushan@stratalabs.in>
2011-02-17 11:17:32 +0530

    Part Payment changes

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -9,7 +9,7 @@ class Signup < ActiveRecord::Base
   attr_accessor :check_no, :card_no
   attr_accessible :name, :address, :mphone, :lphone, :email, :referrer_member_id, 
     :employee_no, :plan_id, :branch_id, :signup_months, :payment_mode, :membership_no, 
-    :application_no, :coupon_no, :coupon_id, :coupon_amt
+    :application_no, :coupon_no, :coupon_id, :coupon_amt, :paid_amt
   
   belongs_to :user, :foreign_key => 'created_by'
   belongs_to :user, :foreign_key => 'modified_by'
@@ -22,15 +22,17 @@ class Signup < ActiveRecord::Base
   validates :referrer_member_id, :length => { :maximum => 10 }
   validates :employee_no, :length => { :maximum => 10 }
   validates :membership_no, :presence => true, :uniqueness => true, :length => { :is => 7 }
-#  validates :application_no, :presence => true, :uniqueness => true, :length => { :maximum => 10 }
+  validates :paid_amt, :presence => true, :numericality => { :greater_than => 0 }
+# validates :application_no, :presence => true, :uniqueness => true, :length => { :maximum => 10 }
   validates :payment_mode, :presence => true, :numericality => {:greater_than => 0, :less_than => 4 }
   validates :email, :email => true
   validate :payment_ref_should_not_be_blank
   validate :atleast_one_phone_number_required
   validate :coupon_no_should_not_be_blank
+  validate :paid_amt_greater_than_bill_amt
   
   before_save :set_defaults
-  
+    
   def coupon_no_should_not_be_blank
       if coupon_no.blank?
         errors.add(:coupon_no, "should not be blank") unless coupon_id.nil?
@@ -60,6 +62,12 @@ class Signup < ActiveRecord::Base
       end
     end
   end
+
+  def paid_amt_greater_than_bill_amt
+    plan = Plan.find(plan_id)
+    bill_amt = plan.total_due_for_term(signup_months, coupon_id)
+    errors.add(:paid_amt, "is more than Bill Amount") if paid_amt.to_d > bill_amt.to_d
+  end
    
   private 
   
@@ -84,9 +92,18 @@ class Signup < ActiveRecord::Base
     self.security_deposit = plan.security_deposit
     self.registration_fee = plan.registration_fee
     self.reading_fee = plan.reading_fee_for_term(self.signup_months)
-    self.advance_amt = 0
-    self.paid_amt = self.security_deposit + self.registration_fee + self.reading_fee - (self.discount + self.coupon_amt) + self.advance_amt
+    self.advance_amt = 0    
     self.overdue_amt = 0
+
+    #part payment starts
+    bill_amt = self.security_deposit + self.registration_fee + self.reading_fee - (self.discount + self.coupon_amt)    
+    actual_paid_amt = self.paid_amt
+    
+    if bill_amt > actual_paid_amt
+      self.overdue_amt = bill_amt - actual_paid_amt
+    end
+    #part payment ends    
+    
     self.start_date = Time.zone.today
     if plan.subscription
       self.expiry_date = self.start_date.months_since(self.signup_months)
