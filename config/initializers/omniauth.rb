f21a9555d3b68f253ae114b5dbc8f4a063cea7ae
akhilesh <kataria.subs@gmail.com>
2011-03-21 11:37:22 +0530

    wanorier's email domain is wan and not wnr

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -22,7 +22,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :name => 'rrn', :domain => 'rrn.justbooksclc.com'
 
   provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :name => 'skn', :domain => 'skn.justbooksclc.com'
-  provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :name => 'wnr', :domain => 'wnr.justbooksclc.com'
+  provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :name => 'wan', :domain => 'wan.justbooksclc.com'
   
   provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :name => 'strata', :domain => 'strata.co.in'
   
